48158759998c955fa18d8db4424fd1ba06805c4f
Rakibulislamrabby <rakibulislamrabby64@gmai.com>
2022-04-18 01:33:39 +0600

    add banner section

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -19,8 +19,7 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto ">
-                            <Nav.Link href="home#services">Services</Nav.Link>
-                            <Nav.Link href="home#need">Need</Nav.Link>
+
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="blog">Blogs</Nav.Link>
--- src/component/Home/Banner/Banner.js
+++ src/component/Home/Banner/Banner.js
@@ -8,10 +8,18 @@ import bg from "../../../images/banner/bg1.jpg"
 
 const Banner = () => {
     return (
-        <div className='container mt-5'>
+        <div className='container pt-5'>
             <div className="row ">
-                <div className="col-lg-8 mt-5">
-                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, non.</h2>
+                <div className="col-lg-8  ">
+                    <div>
+                        <h2 className='Header-title'>Hey <span className='text-success fw-bold'>I am Nick Mitchell</span></h2>
+                        <h2 className='Header-title'>Your Personal Health Coach</h2>
+                    </div>
+                    <div>
+                        <p className='Header-desc'>Work towards your fitness goal with one amazing personal trainer.Evreytime Your Goal is My Goal.Suffering physically and mentally.Seeing darkness all around in depression?Can't lose weight even after trying hard to lose weight? Then hand over your health and plan to Nick Mitchell. For the Price of just one of cup coffee per week.</p>
+                    </div>
+                    <a href="https://en.wikipedia.org/wiki/Nick_Mitchell" target="_blank"> <button className='btn btn-success  px-4 mb-4'>Starting Journey</button></a>
+
                 </div>
                 <div className="col-lg-4">
                     <img className='rounded' style={{ width: "300px" }} src={bg} alt="" />
